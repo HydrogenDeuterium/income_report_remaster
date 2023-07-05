e811e0adace227b2d4f3b3b0826d9f5f876558e0
deuterium <hydrogendeuterium1@gmail.com>
2023-07-05 18:38:27 +0800

    分离日期相关函数，引入workalendar库辅助计算工作日

---
+++ src/date.py
@@ -0,0 +1,103 @@
+import calendar
+import datetime
+import re
+
+from workalendar.asia import China
+
+from category import filter_input, MonthAndDays
+from util import smart_import
+
+
+def get_months(s: str) -> list[int]:
+    """
+    月份从 1 开始。
+    'yr'->1,2,3,4,5,6,7,8,9,10,11,12
+    'q1'->1,2,3
+    '3'->3
+    'Q3'->7,8,9
+    """
+    if s.startswith(('yr', 'YR', 'yR', 'Yr')):
+        months = list(range(1, 13))
+    elif s.startswith(('q', 'Q')):
+        quarter = int(s[1])
+        months = list(range(quarter * 3 - 2, quarter * 3 + 1))
+    else:
+        months = [int(s)]
+    return months
+
+
+def get_month_days(yearmonth=None, *, test=False, restraint_keys: list[str] = None) -> (int, list[MonthAndDays]):
+    """获取年份与每月工作状态信息信息
+    year,month->year,list[month,dict[dayname,day_count]]
+    """
+    if yearmonth is None:
+        text = filter_input("year&month(YYYYMM):")
+    else:
+        text = yearmonth
+    # 20yr->20+yr 20Q1->20+q1,2011->20+11
+    yearmonth = re.search(r"(\d{4})(\d{2}|[qQ][1-4]|[yY][rR])", text)
+    assert yearmonth, "输入格式不正确！"
+    
+    year = yearmonth.groups()[0]
+    months = get_months(yearmonth.groups()[1])
+    
+    month_and_dayses: list[MonthAndDays] = []
+    
+    # TODO 调整泛用性
+    if len(months) == 1:
+        m = months[0]
+        text = filter_input(f'{m}月出门或在校天数 {m}月在家天数:')
+        search = re.search(r"(\d{1,2}) (\d{1,2})", text)
+        assert search, "输入格式不正确！"
+        zaixiao, zaijia = search.groups()
+        month_and_dayses.append((m, {'在校': zaixiao, '在家': zaijia}))
+    else:
+        for m in months:
+            ast: list[dict] = smart_import(f"{year}月报/{year[-2:]}{m:02}.md", "md")
+            try:
+                for _ in ast:
+                    if _['type'] == 'paragraph':
+                        text = _['children'][0]['raw']
+                        break
+                else:
+                    raise EOFError('输入的文件不正确')
+            except KeyError as e:
+                pass
+            search = re.search(r'本月共 ?(\d+) ?天，其中在校 (\d+) 天，在家 (\d+) 天。', text)
+            assert search
+            total, zaixiao, zaijia = map(int, search.groups())
+            month_and_dayses.append((m, {'default': total - zaixiao - zaijia, '在校': zaixiao, '在家': zaijia}))
+    
+    return int(year), month_and_dayses
+
+
+def get_working_days(year, month):
+    # 获取该月的第一天和最后一天的星期几和天数
+    _, days_in_month = calendar.monthrange(year, month)
+    
+    # 获取该月的第一天和最后一天
+    start_date = datetime.date(year, month, 1)
+    end_date = datetime.date(year, month, days_in_month)
+    
+    # 计算两个日期之间的工作日数量
+    working_days = China().get_working_days_delta(start_date, end_date)
+    
+    # 返回工作日数量
+    return working_days
+
+
+def get_next(last_month_out_homes: list[MonthAndDays]) -> list[tuple]:
+    """预测下一个时间段（月，季，年）在家在校天数"""
+    # TODO 调整泛用性
+    
+    out_map: dict[int, tuple[int, int]] = {
+        1: (23, 8), 2: (16, 12), 3: (31, 0), 4: (30, 0), 5: (31, 0), 6: (30, 0),
+        7: (25, 6), 8: (5, 26), 9: (30, 0), 10: (28, 3), 11: (30, 0), 12: (31, 0),
+    }
+    ret = []
+    last_month_ = last_month_out_homes[-1][0]
+    for i, j in enumerate(last_month_out_homes):
+        new_month = (last_month_ + i) % 12 + 1
+        # ret.append(tuple([new_month] + out_map[new_month]))
+        ret.append((new_month, *out_map[new_month]))
+    return ret
--- src/util.py
+++ src/util.py
@@ -1,6 +1,5 @@
 import contextlib
 import os
-import re
 import tomllib
 from collections import defaultdict
 from decimal import Decimal
@@ -9,62 +8,9 @@ from typing import Any, Type
 import jinja2
 import mistune
 
-from category import Category, filter_input, MonthOutHome
+from category import Category
 
 file_dir = r"C:\Users\Deu\OneDrive\Projects\Python\income_report_data"
-
-
-def get_months(s: str) -> list[int]:
-    """
-    'yr'->1,2,3,4,5,6,7,8,9,10,11,12
-    'q1'->1,2,3
-    '3'->3
-    'Q3'->7,8,9
-    """
-    if s.startswith(('yr', 'YR', 'yR', 'Yr')):
-        months = list(range(1, 13))
-    elif s.startswith(('q', 'Q')):
-        quarter = int(s[1])
-        months = list(range(quarter * 3 - 2, quarter * 3 + 1))
-    else:
-        months = [int(s)]
-    return months
-
-
-def get_month_days(yearmonth=None, *, test=False) -> (int, list[MonthOutHome]):
-    """获取年份与每月外出在家信息"""
-    if yearmonth is None:
-        text = filter_input("year&month(YYYYMM):")
-    else:
-        text = yearmonth
-    # 20yr->20+yr 20Q1->20+q1,2011->20+11
-    yearmonth = re.search(r"(\d{4})(\d{2}|[qQ][1-4]|[yY][rR])", text)
-    assert yearmonth, "输入格式不正确！"
-    
-    year = yearmonth.groups()[0]
-    months = get_months(yearmonth.groups()[1])
-    
-    month_out_homes: list[MonthOutHome] = []
-    
-    for m in months:
-        try:
-            if len(months) == 1:
-                assert False
-            ast: list[dict] = smart_import(f"{year}月报/{year[-2:]}{m:02}.md", "md")
-            text = ast[1]['children'][0]['text']
-            search = re.search(r'本月共 \d+ 天，其中在校 (\d+) 天，在家 (\d+) 天。', text)
-            assert search
-        except (AssertionError, FileNotFoundError):
-            if test:
-                text = '25 5'
-            else:
-                text = filter_input(f'{m}月出门或在校天数 {m}月在家天数:')
-            search = re.search(r"(\d{1,2}) (\d{1,2})", text)
-            assert search, "输入格式不正确！"
-            
-        out, home = search.groups()
-        month_out_homes.append((m, int(out), int(home)))
-    return int(year), month_out_homes
 
 
 def get_last(last_year: int, last_month) -> defaultdict[Any, Type[Decimal]] | dict[Any, Decimal]:
@@ -80,7 +26,7 @@ def get_last(last_year: int, last_month) -> defaultdict[Any, Type[Decimal]] | di
     for i in s[:-1]:
         subs += i['children'][1]['children']
     
-    a = [sub['children'][0]['children'][0]['text'] for sub in subs]
+    a = [sub['children'][0]['children'][0]['raw'] for sub in subs]
     ret = defaultdict(Decimal)
     for ss in a:
         k, v = ss.split(':')
@@ -89,12 +35,18 @@ def get_last(last_year: int, last_month) -> defaultdict[Any, Type[Decimal]] | di
 
 
 def smart_import(filename, ext=None) -> str | dict | list[dict]:
+    """
+    ext to return data structure map:
+        toml -> structure data
+        md/markdown -> markdown ast
+        None/other -> plain text
+    """
     if ext is None and len(filename.split('.')) == 2:
         ext = filename.split('.')[-1]
     
     @contextlib.contextmanager
     def smart_open(filename_, *args, **kwargs):
-        prefixes = file_dir,"../", "",'test/'
+        prefixes = file_dir, "../", "", 'test/'
         for prefix in prefixes:
             try:
                 file_path = os.path.join(prefix, filename_)
@@ -127,7 +79,7 @@ def get_structure(last_year: int, last_month: int, toml='./budget.toml', *, test
         items_budgets: dict[str, dict[str, dict]] = smart_import(toml, ext='toml')['预算']
     except FileNotFoundError:
         items_budgets: dict[str, dict[str, dict]] = smart_import('budget/budget.toml', ext='toml')['预算']
-        
+    
     last_data = get_last(last_year, last_month)
     
     categories = []
@@ -152,19 +104,3 @@ def get_template(filename):
     except jinja2.exceptions.TemplateNotFound:
         return jinja2.Environment(loader=jinja2.FileSystemLoader(
             "../template")).get_template(filename)
-
-
-def get_next(last_month_out_homes: list[MonthOutHome]) -> list[tuple]:
-    """预测下一个时间段（月，季，年）在家在校天数"""
-    # TODO 已知问题：无法处理闰年情况 预计 2023 年末修复
-    out_map: dict[int, tuple[int, int]] = {
-        1: (23, 8), 2: (16, 12), 3: (31, 0), 4: (30, 0), 5: (31, 0), 6: (30, 0),
-        7: (25, 6), 8: (5, 26), 9: (30, 0), 10: (28, 3), 11: (30, 0), 12: (31, 0),
-    }
-    ret = []
-    last_month_ = last_month_out_homes[-1][0]
-    for i, j in enumerate(last_month_out_homes):
-        new_month = (last_month_ + i) % 12 + 1
-        # ret.append(tuple([new_month] + out_map[new_month]))
-        ret.append((new_month, *out_map[new_month]))
-    return ret
